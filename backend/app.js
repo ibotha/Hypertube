1fa7f54622b12c248f8bd4fe9baa9f8042d95ed7
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2019-01-09 14:14:31 +0200

    My Init

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const pageRoutes = require('./routes/pages');
+const session = require('express-session');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({secret: "American Pie: Beta House", saveUninitialized: false, resave: false}));
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+	  "Access-Control-Allow-Headers",
+	  "Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.setHeader(
+	  "Access-Control-Allow-Methods",
+	  "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+	);
+	next();
+});
+
+app.use("/", pageRoutes);
+
+module.exports = app;
