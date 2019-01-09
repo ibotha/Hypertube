c773f628dfe90adb6fe196382f8cd6e63de534bd
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2019-01-09 14:30:03 +0200

    auto commit

--- backend/routes/general.js
+++ backend/routes/general.js
@@ -1,7 +1,13 @@
 const express		= require('express');
 const router		= express.Router();
 
+router.post('*', function(req, res, next) {
+	//Technically a 404
+	res.end(404);
+})
+
 router.get('*', function(req, res, next) {
+	//Technically a 404
 	res.end('{"Msg":"OK"}');
 });
 
