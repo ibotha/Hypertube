dd2aad2eee66ad6274d01fa1393c7010393e8c72
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2019-01-10 12:29:34 +0200

    Pages

---
+++ src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+
+export class HomePageComponent implements OnInit {
+  ngOnInit() {}
+}
